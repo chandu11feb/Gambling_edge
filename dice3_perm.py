991aeb490295d04e14efb7ae33c912492e1e4a1b
chandu <62502194+chandu11feb@users.noreply.github.com>
2022-12-21 18:58:30 +0530

    Add files via upload

---
+++ dice3_perm.py
@@ -0,0 +1,19 @@
+import itertools
+import collections
+dice = [1, 1, 1, 2,2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]
+p = []
+su = []
+l = set(itertools.permutations(dice, 3))
+k = list(l)
+p = p + k
+# print(p)
+p.sort()
+print(*p, sep="\n")
+for i in p:
+    # print(sum(i))
+    su.append(sum(i))
+su.sort()
+print(*su)
+sum_stats = collections.Counter(su)
+print(sum_stats)
+print(len(p))
